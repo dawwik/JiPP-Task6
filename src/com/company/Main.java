3f02ceaa333bc9c20455138533c362c30f9c479d
dawwik <dawid.wiktorowski22@gmail.com>
2020-06-02 22:12:24 +0200

    Add average

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double average;
+        ArrayList <Double> list = new ArrayList<>();
+        list.add(2.2);
+        list.add(3.3);
+        list.add(4.4);
+
+
+        average = list.get(0)+list.get(1)+list.get(2);
+        System.out.println("Srednia liczb 2.2, 3.3, 4.4 to: " + average + " a mediana tych liczb wynosi: ");
     }
 }
