d1ad975b33056cb08da3cd1da22e221a0a59f73a
dawwik <dawid.wiktorowski22@gmail.com>
2020-06-03 18:17:03 +0200

    Adding new function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,36 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        double average;
-        ArrayList <Double> list = new ArrayList<>();
-        list.add(2.2);
-        list.add(3.3);
-        list.add(4.4);
+        Scanner scanner = new Scanner(System.in);
+        List<Double> list = new ArrayList<>();
+        double next = 0;
+        boolean numberOfNumbers = true;
+        int licznik = 0;
+        System.out.println("Podaj 10 liczb ");
+        while (numberOfNumbers) {
+            next = scanner.nextDouble();
+
+            if (licznik <4) {
+                list.add(next);
+                licznik++;
+            } else
+                numberOfNumbers = false;
+        }
 
 
-        average = list.get(0)+list.get(1)+list.get(2);
-        System.out.println("Srednia liczb 2.2, 3.3, 4.4 to: " + average + " a mediana tych liczb wynosi: ");
+        double sum = 0;
+        for (Double numbers : list) {
+            sum += numbers;
+        }
+        double avgerage = sum / list.size();
+
+        System.out.println("Suma tych liczb wynosi: " + sum);
+        System.out.println("Srednia wynosi: " + avgerage);
     }
 }
