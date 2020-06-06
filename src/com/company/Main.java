67451344ecc36223485822f0164d53c620ff5a0d
dawwik <dawid.wiktorowski22@gmail.com>
2020-06-06 10:48:35 +0200

    Refactor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
         List<Double> list = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 9; i++){
             list.add((double)i);
         }
         Collections.shuffle(list);
 
-        System.out.println("Losowa kolejność liczb z przedzialu [ 0 - 10 ] to: ");
+        System.out.println("Losowa kolejność liczb z przedzialu [ 0 - 9 ] to: ");
         System.out.println(list);
 
 
@@ -29,8 +29,8 @@ public class Main {
         double avgerage = sum / list.size();
 
         double median;
-        if (list.size() % 2 == 1)
-            median = (list.get(list.size() / 2) + list.get(list.size() / 2 +1))/2;
+        if (list.size() % 2 == 0)
+            median = (list.get(list.size() / 2) + (list.get(list.size()/ 2)+1))/2;
          else
             median = list.get(list.size() / 2);
             System.out.println(median);
